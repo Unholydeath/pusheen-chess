87313a16eafa7f14bc8a319c6bdeb9f3cccecc47
Braden Bodily <thebodily@gmail.com>
2019-08-22 00:41:51 -0600

    Changes to find if a king is in check

--- src/actions/handleTileClick.js
+++ src/actions/handleTileClick.js
@@ -1,4 +1,6 @@
 import { movePiece, setActiveTile } from './helpers';
+import isKingInCheck from './helpers/isKingInCheck';
+import getKingTile from './helpers/getKingTile';
 
 /**
  * Either sets a tile to active or moves the active piece to this tile.
@@ -10,8 +12,20 @@ import { movePiece, setActiveTile } from './helpers';
 const handleTileClick = (state, tileId) => {
   if (state.activeTile !== null && !state.legalMoves.includes(tileId)) {
     return { activeTile: null, legalMoves: [] };
+    
   } else if (state.activeTile !== null) {
-    return movePiece(state, { from: state.activeTile, to: tileId });
+
+    var result = movePiece(state, { from: state.activeTile, to: tileId });
+    const [newRow, newCol] = tileId.split('');
+    var enemyKingColor;
+    result.board[newRow][newCol].team === 0 ? enemyKingColor = 1 : enemyKingColor = 0;
+    var kingTile = getKingTile(result, enemyKingColor)
+
+    if(isKingInCheck(result, kingTile)){
+      console.log("CHECK!!!!");
+    }
+
+    return result;
   }
 
   return setActiveTile(state, tileId);
